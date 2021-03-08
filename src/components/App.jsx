12c9209c31ff40d77296e4c785c2b17201dcb4a7
Vaibhav Chauhan <chauhanvaibhav095@gmail.com>
2021-03-09 01:10:50 +0530

    created components folder and moved jsx files

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Inv_form from "../inv-from"
+import Supplier_form from "../supplier-form"
+
+function App(){
+
+    return (<div>
+        <h1>Hello World!</h1>
+        <Inv_form />
+        <Supplier_form />
+    </div>);
+}
+
+export default App;
