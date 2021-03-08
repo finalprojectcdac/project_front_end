f8dd76eb99da42fa4d0563651da71a1f4621a16b
sagar0723 <sagarsahuofficial@gmail.com>
2021-03-09 01:27:45 +0530

    change 4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,15 @@ import Supplier_form from "../supplier-form"
 function App(){
 
     return (<div>
-<<<<<<< HEAD:src/App.jsx
+
         <h1>Hello World! sagar 123 shau</h1>
-=======
+
 
         <h1>vaibhav hello sandipan</h1>
 
         <h1>Hello World!</h1>
 
->>>>>>> sandipan:src/components/App.jsx
+
         <Inv_form />
         <Supplier_form />
     </div>);
