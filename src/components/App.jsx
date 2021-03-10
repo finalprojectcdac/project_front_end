d45c052bd6232b4fdf4fd364e6b0620daa626e85
finalprojectcdac <finalprojectcdac@gmail.com>
2021-03-10 22:08:21 +0530

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Inv_form from "../inv-from"
-import Supplier_form from "../supplier-form"
+
+import Form  from "../components/Form"
+import Table  from "../components/Table"
 
 function App(){
 
+
     return (<div>
-
-        <h1>vaibhav hello sandipan</h1>
-
-        <h1>Hello World!</h1>
-
-        <Inv_form />
-        <Supplier_form />
+        <Form />
+        <Table />
     </div>);
 }
 
