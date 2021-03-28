6716007277efa3dd401065ecdaf295ac94fa9128
Vaibhav Chauhan <chauhanvaibhav095@gmail.com>
2021-03-29 02:20:06 +0530

    quantity bug in billing fixed by sagar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,73 @@
-import React from "react";
+import React ,{useState} from "react";
 import HorizontalNavbar from "../components/general-components/HorizontalNavbar";
+import {Link} from "react-router-dom";
+import "../home.css";
+import { Button } from "./Button";
+import HomePageBodySection from "./HomePageBodySection";
+
 
 function HomePage() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+    const handleClick = () => setClick(!click);
+
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setButton(false);
+    }
+    else{
+      setButton(true);
+    }
+  };
+  
+  window.addEventListener('resize',showButton)
+
   return (
-    <div>
-      <HorizontalNavbar />
-      <img src="https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80" />
-      {/* login
-      register */}
+    <div className="homepage">
+      {/* <style>{'body { background-color: green; }'}</style> */}
+
+      {/* <div>
+        <div className="heading">
+          <h1 className="h1">Inventory</h1>
+        </div>
+        <img id="img" src="images/logo.jpg"></img>
+        
+
+        <div className="container">
+          <div className="center"></div>
+          <ul>
+            <li>
+              <a href="">LOGIN</a>
+            </li>
+            <li>
+              <a href="">REGISTER</a>
+            </li>
+          </ul>
+        </div>
+      </div> */}
+
+      <nav className ="navbar">
+        <div className ="navbar-container">
+          <Link to = "/" className = "navbar-logo">
+            IBM <i className='fab fa-typo3'></i>
+          </Link>
+          <div className = "menu-icon" onClick = {handleClick}>
+            <i className = {click? 'fas fa-times' : 'fas fa-bars' }></i>
+          </div>
+          <ul className = {click? 'nav-menu active' : 'nav-menu'}>
+            <li className = "nav-item">
+              <Link to = "/" className ="nav-links" onClick ={closeMobileMenu}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+          {button && <Button buttonStyle='btn--outline'>ABOUT US</Button>}
+        </div>
+      </nav>
+      <HomePageBodySection />
+
     </div>
   );
 }
