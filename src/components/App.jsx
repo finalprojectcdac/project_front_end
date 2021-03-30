e70e945e62cd93cc95628f651bf7d623ec556916
finalprojectcdac <finalprojectcdac@gmail.com>
2021-03-30 16:19:07 +0530

    homepage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ function App() {
     <div>
       <Router>
         <Switch>
-          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/" component={HomePage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/register" component={RegisterPage} />
           <ProtectedRoute exact path="/welcome" component={WelcomePage} />
