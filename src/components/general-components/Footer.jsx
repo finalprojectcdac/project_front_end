0f430c3742a9d685766be44c3afe0eb6db8cd861
finalprojectcdac <finalprojectcdac@gmail.com>
2021-04-12 08:35:18 +0530

    FOOTER ADDED

--- src/components/general-components/Footer.jsx
+++ src/components/general-components/Footer.jsx
@@ -11,22 +11,22 @@ function Footer() {
         <small class="website-rights">IBS © 2021</small>
         <div class="social-icons">
         {/* <div class="footer-logo social-logo"> */}
-        <small class="website-rights">Developer</small>
+        <small class="website-rights">Developers</small>
         {/* </div> */}
           <div class="social-icon-link ">
-          <a href="https://www.google.com" style={{textDecoration:"none",color:"inherit"}}><i class="fab fa-linkedin"></i></a> 
+          <a href="https://www.linkedin.com/in/maheshwari-modala-0bb05517b" style={{textDecoration:"none",color:"inherit"}}><i class="fab fa-linkedin"></i></a> 
           </div>
           <div class="social-icon-link ">
-            <i class="fab fa-linkedin" />
+          <a href="https://www.linkedin.com/in/shubham-sharma-a8476a191" style={{textDecoration:"none",color:"inherit"}}><i class="fab fa-linkedin"></i></a> 
           </div>
           <div class="social-icon-link ">
-            <i class="fab fa-linkedin" />
+          <a href="https://www.linkedin.com/in/sagar-sahu-612a0a165" style={{textDecoration:"none",color:"inherit"}}><i class="fab fa-linkedin"></i></a> 
           </div>
           <div class="social-icon-link ">
-            <i class="fab fa-linkedin" />
+          <a href="https://www.linkedin.com/in/chauhanvaibhav095" style={{textDecoration:"none",color:"inherit"}}><i class="fab fa-linkedin"></i></a> 
           </div>
           <div class="social-icon-link ">
-            <i class="fab fa-linkedin" />
+          <a href="https://www.linkedin.com/in/sandipan-bera-61a9a4184" style={{textDecoration:"none",color:"inherit"}}><i class="fab fa-linkedin"></i></a> 
           </div>
         </div>
       </div>
